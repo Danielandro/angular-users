3ba0a040eaad0ae458ad3b753fb9cc4a40a94e23
Danny <mtme13@hotmail.com>
2020-01-23 13:29:44 +0000

    chore: update page title, imports and add router-outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./components/home/home.component";
 import { UsersComponent } from "./components/users/users.component";
@@ -9,7 +10,13 @@ import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, UsersComponent],
-  imports: [BrowserModule, FormsModule, RouterModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
