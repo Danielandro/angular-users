a7ced230c8ca49d744bcf6972fe880fb1d8675ef
Danny <mtme13@hotmail.com>
2020-01-23 16:14:46 +0000

    feat: Get correct user on user detail page

--- src/app/components/users/user-detail/user-detail.component.ts
+++ src/app/components/users/user-detail/user-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { IUser } from "src/app/models/user";
 import { UserService } from "../../../services/user.service";
 import { Observable } from "rxjs";
+import { Router, ActivatedRoute } from "@angular/router";
+import { switchMap } from "rxjs/operators";
 
 @Component({
   selector: "app-user-detail",
@@ -12,9 +14,18 @@ export class UserDetailComponent implements OnInit {
   pageTitle: string = "User Details";
   user$: Observable<IUser>;
   errorMessage: string = "Unable to retrieve user";
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.user$ = this.userService.getUser(1);
+    // get id from route & pass to UserService to get User
+    this.user$ = this.route.paramMap.pipe(
+      switchMap(params => {
+        return this.userService.getUser(+params.get("id"));
+      })
+    );
   }
 }
